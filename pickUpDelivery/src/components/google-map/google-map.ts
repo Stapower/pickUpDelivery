ed4a2a9fcaa48bf8cf28910cb2a37c27ad3bc775
stapower <tomynfs2@gmail.com>
2018-12-30 01:34:45 -0300

    working with geolocation. start and end of journey

--- pickUpDelivery/src/components/google-map/google-map.ts
+++ pickUpDelivery/src/components/google-map/google-map.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 
+declare var google;
+ 
 /**
  * Generated class for the GoogleMapComponent component.
  *
@@ -17,18 +19,18 @@ export class GoogleMapComponent {
   text: string;
 
   constructor() {
-    console.log("constructor: " + this.map);
-
   }
 
   ngOnInit(){
-    console.log("ionViewDidLoad: " + this.map);
-
     this.initMap();
   }
 
   initMap(){
-    console.log("initMap: " + this.map);
+    
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+    this.setCurrentPosition();
 
     let coords = new google.maps.LatLng(45,100);
     let mapOptions: google.maps.MapOptions = {
@@ -38,7 +40,91 @@ export class GoogleMapComponent {
     }
 
     this.map = new google.maps.Map(this.mapHtmlElement.nativeElement, mapOptions)
-    console.log("map: " + this.map);
+
+    directionsDisplay.setMap(this.map);
+    
+    var geocoder = new google.maps.Geocoder();
+    var address = "uspallata 309, claypole, buenos aires, argentina";
+
+/*     console.log("before geocode");
+    geocoder.geocode( { 'address': address}, function(results, status) {
+    console.log("after geocode");
+
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+        alert(latitude);
+        } 
+    });  */
   }
 
+  //Uses the google object GeocoderResult:
+  /* GeocoderResult:  
+    results[]: {
+        types[]: string,
+        formatted_address: string,
+        address_components[]: {
+          short_name: string,
+          long_name: string,
+          postcode_localities[]: string,
+          types[]: string
+        },
+        partial_match: boolean,
+        place_id: string,
+        postcode_localities[]: string,
+        geometry: {
+          location: LatLng,
+          location_type: GeocoderLocationType
+          viewport: LatLngBounds,
+          bounds: LatLngBounds
+        }
+      }
+    }
+  */
+  setCurrentPosition(){
+    //currentPosition : any; this should be object geocoderRESULT and i should return this object.
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      },
+      function(results, status) {
+        alert(results[0].formatted_address);
+        if (status == google.maps.GeocoderStatus.OK)
+        (<HTMLInputElement>document.getElementById("start")).value = results[0].formatted_address;
+        results[0].
+        else
+          console.log("error retrieving adress");
+          //$("#error").append("Unable to retrieve your address<br />");
+      });
+    },
+    function(positionError){
+      console.log("internal error retrieving adress: " + positionError.message);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000 // 10 seconds
+    });
+
+  }
+
+  
+
+/*     var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      directionsService.route({
+        origin: document.getElementById('start').nodeValue,
+        destination: document.getElementById('end').nodeValue,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    } */
 }
